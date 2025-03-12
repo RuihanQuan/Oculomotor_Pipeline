b1c0e776128335208bfabfd0ce3a33a14b976007
RUIHANC <rquan4@jh.edu>
2025-03-11 23:31:34 -0700

    3/11/2025

--- filter test/template_subtraction_ICA.m
+++ filter test/template_subtraction_ICA.m
@@ -1,9 +1,12 @@
-function amplifier_data_copy = template_subtraction(amplifier_data, trigs, chan, params)
+function amplifier_data_copy = template_subtraction_ICA(amplifier_data, trigs, chan, params)
 NSTIM = params.NSTIM;
 start = params.start;
 isstim = params.isstim; % if it is stim channel, we want to blank the signal during stimulus
 period = trigs(2)- trigs(1);
 period_avg = params.period_avg; % 1ms
+if period_avg <= 0 
+   period_avg = period; 
+end
 prebuffer = params.buffer;
 chn_data = zeros(NSTIM, period+prebuffer);
 rhs_name = params.name;
@@ -21,32 +24,31 @@ for i = 1:NSTIM
     chn_data(i, 1:length(segment)) = amplifier_data(segment); 
 end
 template1 = chn_data;
-X = chn_data(temp, 1:period_avg+prebuffer);
-ricaModel = rica(X', numComponents, 'NonGaussianityIndicator', ones(1, numComponents));
+X = chn_data(temp, :);
+[coeff, score, latent] = pca(X);
 
-% Get the transformation matrix (weights)
-W = ricaModel.TransformWeights;  % Size: (numComponents x 77)
+% Keep components explaining 99% variance
+explainedVariance = cumsum(latent) / sum(latent);
+numComponents = find(explainedVariance >0.9, 1);
 
-% Transform data into independent components
-S = transform(ricaModel, X');
+% Reduce and reconstruct
+X_reduced = score(:, 1:end) * coeff(:, 1:end)';
+% template = template(1:period_avg+prebuffer, :)';
 
-weights = sum(abs(W), 1);
-[~, artifactidx] = min(weights);
-S(:,artifactidx) = 0;
-template = S*W';
-template1(temp, 1:period_avg+prebuffer) = template';
+template1(temp, 1:period_avg+prebuffer) = X_reduced(:,  1:period_avg+prebuffer);
 for i = 1:NSTIM
     if ~or(mod(i, num_pulse)==0, mod(i, num_pulse) > skip_n)
-        template1(i, 1:period_avg+prebuffer) = template1(i+skip_n, 1:period_avg+prebuffer);
+%         template(i, 1:period_avg+prebuffer) = template(i+skip_n, 1:period_avg+prebuffer);
+         template1(i, 1:period_avg+prebuffer) = template1(i+skip_n, 1:period_avg+prebuffer);
     end
-    template1(i, 1:period_avg+prebuffer) = template1(i, 1:period_avg+prebuffer)-template1(i, 1) ;
+    template1(i, 1:period_avg+prebuffer) = template1(i, 1:period_avg+prebuffer)-template1(i, 1) ; %  
     template1(i, period_avg+prebuffer:end) = linspace(template1(i,period_avg+prebuffer), 0, period-period_avg+1);
 end
 amplifier_data_copy = amplifier_data;
 for i = 1 : NSTIM
     segment = (-prebuffer+1 + trigs(i) ):min(period+ trigs(i), length(amplifier_data) );
-    amplifier_data_copy( segment) = amplifier_data_copy( segment) - template1(i,1:length(segment));
-  
+    amplifier_data_copy( segment) =  template1(i,1:length(segment)); % amplifier_data_copy( segment) -  
+ 
 end
 
 if isstim 
